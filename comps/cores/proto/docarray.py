d6144e7763ff16465c5a6411fcc3111701e0ab01
BaoHuiling <huiling.bao@intel.com>
2024-08-08 15:31:35 +0800

    add readme, test script, fix bug

--- comps/cores/proto/docarray.py
+++ comps/cores/proto/docarray.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-from typing import Optional, Tuple, Union
+from typing import Dict, List, Optional, Tuple, Union
 
 import numpy as np
 from docarray import BaseDoc, DocList
@@ -72,10 +72,10 @@ class SearchedDoc(BaseDoc):
         json_encoders = {np.ndarray: lambda x: x.tolist()}
 
 class SearchedMultimodalDoc(BaseDoc):
-    retrieved_docs: DocList[TextImageDoc]
+    retrieved_docs: List[TextImageDoc]
     initial_query: str
     top_n: int = 1
-    metadata: Optional[DocList[dict]] = [{}]
+    metadata: Optional[List[Dict]] = None
 
     class Config:
         json_encoders = {np.ndarray: lambda x: x.tolist()}
